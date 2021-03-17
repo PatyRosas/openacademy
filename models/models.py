5f15f7e6a95115c5666082b93612bce4effb77b8
Paty Rosas <pat.uni95@gmail.com>
2021-03-16 23:10:06 -0700

    Add warnings on seats and attendee fields values

--- models/models.py
+++ models/models.py
@@ -18,7 +18,7 @@ class Session(models.Model):
 
 	name = fields.Char(required=True)
 	start_date = fields.Date(default=fields.Date.today)
-	duration = fields.Float(digits=(6,2), help="Duration Sin days")
+	duration = fields.Float(digits=(6,2), help="Duration in days")
 	seats = fields.Integer(string="Number of seats")
 	active = fields.Boolean(default=True)
 
@@ -37,3 +37,20 @@ class Session(models.Model):
 				r.taken_seats = 0.0
 			else:
 				r.taken_seats = 100.0 * len(r.attendee_ids) / r.seats
+
+	@api.onchange('seats', 'attendee_ids')
+	def _verify_valid_seats(self):
+		if self.seats < 0:
+			return {
+				'warning': {
+					'title': "Incorrect 'seats' value",
+					'message': "The number of available seats may not be negative",
+				},
+			}
+		if self.seats < len(self.attendee_ids):
+			return{
+				'warning': {
+					'title': "Too many attendees",
+					'message': "Increase seats or remove excess attendees",
+				},
+			}
